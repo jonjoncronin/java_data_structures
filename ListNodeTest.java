6cbd15a45d5f2f10db176934bfb366e538cf811d
Jon Cronin <jonjoncronin@gmail.com>
2016-06-07 21:49:22 -0700

    added linkedList with test

---
+++ ListNodeTest.java
@@ -0,0 +1,118 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * @author joncronin
+ *
+ */
+public class ListNodeTest
+{
+
+   /**
+    * Test method for {@link ListNode#equals(ListNode)}.
+    */
+   
+   @Test
+   public void testEquals()
+   {
+      Integer anObject = new Integer(1);
+      Integer otherObject = new Integer(2);
+      ListNode someNode = new ListNode();
+      ListNode otherNode = new ListNode();
+      assertTrue(otherNode.equals(someNode) == true);
+      someNode.setNodeData(anObject);
+      assertTrue(otherNode.equals(someNode) == false);
+      otherNode.setNodeData(otherObject);
+      assertTrue(otherNode.equals(someNode) == false);
+   }
+   
+   /**
+    * Test method for {@link ListNode#ListNode()}.
+    */
+   @Test
+   public void testListNode()
+   {
+      ListNode someNode = new ListNode();
+      assertTrue(someNode.data == null);
+      assertTrue(someNode.next == null);
+   }
+
+   /**
+    * Test method for {@link ListNode#ListNode(Object)}.
+    */
+   @Test
+   public void testListNodeInt()
+   {
+      Integer someObject = new Integer(5);
+      ListNode someNode = new ListNode();
+      someNode = new ListNode(someObject);
+      assertTrue(someNode.data == someObject);
+      assertTrue((Integer)(someNode.data) == 5);
+      assertTrue(someNode.next == null);
+   }
+
+   /**
+    * Test method for {@link ListNode#ListNode(Object, ListNode)}.
+    */
+   @Test
+   public void testListNodeIntListNode()
+   {
+      Integer someObject = new Integer(5);
+      ListNode otherNode = new ListNode();
+      ListNode someNode = new ListNode(someObject,otherNode);
+      assertTrue(someNode.data == someObject);
+      assertTrue((Integer)(someNode.data) == 5);
+      assertTrue(someNode.next == otherNode);
+   }
+
+   /**
+    * Test method for {@link ListNode#getNodeData()}.
+    */
+   @Test
+   public void testGetNodeData()
+   {
+      Integer someObject = new Integer(5);
+      ListNode someNode = new ListNode(someObject);
+      assertTrue(someNode.getNodeData() == someObject);
+   }
+
+   /**
+    * Test method for {@link ListNode#setNodeData(int)}.
+    */
+   @Test
+   public void testSetNodeDataInt()
+   {
+      Integer someObject = new Integer(5);
+      ListNode someNode = new ListNode();
+      assertTrue(someNode.setNodeData(someObject) == true);
+      assertTrue(someNode.getNodeData() == someObject);
+   }
+
+   /**
+    * Test method for {@link ListNode#getNodeNext()}.
+    */
+   @Test
+   public void testGetNodeNext()
+   {
+      Integer someObject = new Integer(5);
+      ListNode someNode = new ListNode();
+      ListNode otherNode = new ListNode(someObject,someNode);
+      assertTrue(otherNode.getNodeNext() == someNode);
+   }
+
+   /**
+    * Test method for {@link ListNode#setNodeNext(ListNode)}.
+    */
+   @Test
+   public void testSetNodeNext()
+   {
+      ListNode someNode = new ListNode();
+      ListNode otherNode = new ListNode();
+      assertTrue(otherNode.setNodeNext(someNode) == true);
+      assertTrue(otherNode.getNodeNext() == someNode);
+      assertTrue(otherNode.setNodeNext(otherNode) == false);
+      assertTrue(otherNode.getNodeNext() == someNode);
+   }
+   
+
+}
---
+++ linkedList.java
@@ -0,0 +1,88 @@
+
+public class linkedList
+{
+   int length;
+   int maxSize;
+   ListNode head;
+   ListNode tail;
+
+   public linkedList()
+   {
+      this.length = 0;
+      this.maxSize = 10;
+      this.head = null;
+      this.tail = null;
+   }
+
+   public linkedList(int maxSize)
+   {
+      this.length = 0;
+      this.maxSize = maxSize;
+      this.head = null;
+      this.tail = null;
+   }
+
+   public int getMaxSize()
+   {
+      return this.maxSize;
+   }
+
+   public int getLength()
+   {
+      return this.length;
+   }
+
+   public int addEntryToHead(ListNode someNode)
+   {
+      if(someNode == null)
+      {
+         return -1;
+      }
+      if(this.length < this.maxSize)
+      {
+         if(someNode.setNodeNext(this.head))
+         {
+            this.head = someNode;
+            this.length++;
+            return 1;   
+         }
+         else
+         {
+            return -1;
+         }
+      }
+      else
+      {
+         return 0;
+      }
+   }
+   
+   public int addEntryToTail(ListNode someNode)
+   {
+      if(someNode == null)
+      {
+         return -1;
+      }
+      if(someNode.getNodeNext() != null)
+      {
+         return -1;
+      }
+      if(this.length < this.maxSize)
+      {
+         if(this.tail.setNodeNext(someNode))
+         {
+            this.tail = someNode;
+            this.length++;
+            return 1;
+         }
+         else
+         {
+            return -1;
+         }
+      }
+      else
+      {
+         return 0;
+      }
+   }
+}
