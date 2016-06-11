289a5020482ad77d40d2e87e06c012a8e07ec75b
Jon Cronin <jonjoncronin@gmail.com>
2016-06-11 12:50:14 -0700

    Added javadoc comments

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -1,5 +1,11 @@
 package linkedList;
 
+/**
+ * The LinkedList class is a very simple LinkedList implementation that utilizes
+ * the ListNode class that is also part of the linkedList package.
+ * 
+ * @author Jon Cronin and Scott McDonald
+ */
 public class LinkedList
 {
    int length;
@@ -7,6 +13,9 @@ public class LinkedList
    ListNode head;
    ListNode tail;
 
+   /**
+    * Basic constructor of a LinkedList object
+    */
    public LinkedList()
    {
       this.length = 0;
@@ -15,6 +24,13 @@ public class LinkedList
       this.tail = null;
    }
 
+   /**
+    * Overloaded constructor to allow you to specify the maxSize of the 
+    * LinkedList you want to create.
+    * 
+    * @param maxSize The maxSize of the list you want to create. Only supports 
+    * positive numbers. If you maxSize specified then the max list size is 10.
+    */
    public LinkedList(int maxSize)
    {
       this.length = 0;
@@ -27,16 +43,35 @@ public class LinkedList
       this.tail = null;
    }
 
+   /**
+    * Accessor function to get the max size of the LinkedList.
+    * 
+    * @return The Integer maxSize of the list - how many ListNodes could 
+    * possibly be stored in the LinkedList.
+    */
    public int getMaxSize()
    {
       return this.maxSize;
    }
 
+   /**
+    * Accessor function to get the current length of the list.
+    * 
+    * @return The Integer length of the list - how many ListNodes are currently 
+    * in the LinkedList.
+    */
    public int getLength()
    {
       return this.length;
    }
 
+   /**
+    * Modifier function to add a ListNode to the head of the LinkedList.
+    * 
+    * @param someNode The ListNode you want to add to the LinkedList
+    * @return The Integer length of the list after adding the ListNode. 0 if 
+    * the list is full. -1 if the ListNode.next is not empty.
+    */
    public int addEntryToHead(ListNode someNode)
    {
       if(someNode.getNodeNext() != null)
@@ -62,6 +97,13 @@ public class LinkedList
       }
    }
    
+   /**
+    * Modifier function to add a ListNode to the tail of the LinkedList.
+    * 
+    * @param someNode The ListNode you want to add to the LinkedList
+    * @return The Integer length of the list after adding the ListNode. 0 if 
+    * the list is full. -1 if the ListNode.next is not empty.
+    */
    public int addEntryToTail(ListNode someNode)
    {
       if(someNode.getNodeNext() != null)
@@ -95,7 +137,11 @@ public class LinkedList
          return 0;
       }
    }
-   
+   /**
+    * Overriden function to return a string describing this LinkedList.
+    * @return String in the format of "This list of size [length] contains\n
+    * Data: [Object.toString]" 
+    */
    public String toString()
    {
       ListNode curNode = this.head;
@@ -117,6 +163,14 @@ public class LinkedList
    /*
     * equals checks to make sure that the 2 lists are exactly the same - 
     * same length, same maxSize and same order of elements
+    */
+   /**
+    * Overloaded function to allow you to see if a LinkedList is equal to 
+    * another LinkedList. Equality is based on the length and max sizes of the 
+    * lists as well as on the order of ListNode elements and the equality of the
+    * Objects stored in the ListNodes.
+    * @param someList The list you want to compare this one to.
+    * @return true if the lists equal each other.
     */
    public boolean equals(LinkedList someList)
    {
