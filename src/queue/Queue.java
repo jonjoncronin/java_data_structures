9fcd425399c69a1658ff89f2de83c83e40746a5b
Scott McDonald <mcdonals@gmail.com>
2016-06-11 22:11:16 -0700

    Added enque, deque, getCapacity and getSize methods to the Queue class.

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -10,5 +10,50 @@ package queue;
  * @author mcdonasc
  */
 public class Queue {
+    protected QueueNode head;
+    protected QueueNode tail;
+    protected int capacity;
+    protected int size;
+    
+    public Queue() {
+        head = null;
+        tail = null;
+        capacity = 10;
+        size = 0;
+    }
+    
+    public void enque(QueueNode node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.setNextNode(node);
+            tail = node;
+        }
+        size++;
+    }
+    public QueueNode deque() {
+        if (head == null) {
+            return null;
+        } 
+        
+        QueueNode target = head;
+        if (size > 1) {
+            head = head.getNextNode();
+        } else {
+            head = null;
+        }
+        
+        size--;
+        return target;
+    }
+    
+    public int getCapacity() {
+        return capacity;
+    }
+    
+    public int getSize() {
+        return size;
+    }
     
 }
